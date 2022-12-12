cbcd333f8ca94eb66a31d51e248d50c86cdffd35
LorenzoBondan <lbbondan@gmail.com>
2022-12-12 15:30:44 -0300

    Refactor pageable

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -4,8 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,22 +37,13 @@ public class ProductResource {
 	
 	// TRAZENDO TUDO DE FORMA PAGINADA
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(
-			
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-
-			)
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable)
 	{
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<ProductDTO> list = service.findAllPaged(pageRequest);	
+		Page<ProductDTO> list = service.findAllPaged(pageable);	
 		return ResponseEntity.ok().body(list);
 	}
 	
-	//TRAZENDO CATEGORIA POR ID
+	//TRAZENDO PRODUTO POR ID
 	
 	@GetMapping(value = "/{id}") //ARGUMENTO ACRESCENTADO NA FRENTE DA ROTA BÁSICA
 	// /CATEGORIES/ID
@@ -64,7 +53,7 @@ public class ProductResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	// INSERINDO NOVA CATEGORIA
+	// INSERINDO NOVO PRODUTO
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert (@RequestBody ProductDTO dto)
 	{
@@ -74,7 +63,7 @@ public class ProductResource {
 		return ResponseEntity.created(uri).body(dto);	
 	}
 	
-	// ATUALIZANDO CATEGORIA -> É UMA MISTURA DO BUSCA POR ID COM POST
+	// ATUALIZANDO PRODUTO -> É UMA MISTURA DO BUSCA POR ID COM POST
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto)
 	{
@@ -82,7 +71,7 @@ public class ProductResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	// DELETANDO CATEGORIA
+	// DELETANDO PRODUTO
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> delete(@PathVariable Long id)
 	{
