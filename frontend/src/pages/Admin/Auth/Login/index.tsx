b5716326c6437d79a5c44b3d13afb07e8b58c7ae
LorenzoBondan <lbbondan@gmail.com>
2023-01-31 21:56:19 -0300

    Validation fields username and password

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -16,7 +16,7 @@ const Login = () => {
 
     const [hasError, setHasError] = useState(false); // mensagem de erro ao preencher errado (bootstrap alerts)
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: {errors} } = useForm<FormData>(); //formstate errors -> validação dos campos
 
     const onSubmit = (formData : FormData) => {
         requestBackendLogin(formData)
@@ -43,22 +43,30 @@ const Login = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input 
-            {...register("username")}
+            {...register("username", {
+                required: 'Campo obrigatório'
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+          <div className='invalid-feedback d-block'>{errors.username?.message}</div>
         </div>
+
         <div className="mb-2">
           <input
-            {...register("password")}
+            {...register("password", {
+                required: 'Campo obrigatório'
+            })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+          <div className='invalid-feedback d-block' >{errors.password?.message}</div>
         </div>
+
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
         </Link>
