ff45ef9958ae533060fb15d7f8e7ae886c67fe91
LorenzoBondan <lbbondan@gmail.com>
2023-02-02 10:42:48 -0300

    HasAnyRoles

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -116,3 +116,25 @@ axios.interceptors.response.use(function (response) {
 
     return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
   }
+
+  // restringir rotas por tipo de usuário
+  export const hasAnyRoles = (roles : Role[]) : boolean => {
+
+    if(roles.length === 0){
+        return true;
+    }
+
+    const tokenData = getTokenData();
+
+    if(tokenData !== undefined){
+       for (var i = 0; i < roles.length; i++){
+            if(tokenData.authorities.includes(roles[i])){
+                return true;
+            }
+       }
+    }
+
+    //return roles.some(role => tokenData.authorities.includes(role)); // OUTRA FORMA DE PERCORRER A LISTA PARA PROCURAR O ROLE
+
+    return false;
+  }
