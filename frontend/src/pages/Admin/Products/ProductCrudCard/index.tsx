8a85cfcb5fb2564343e4335b09996241e9e87bae
LorenzoBondan <lbbondan@gmail.com>
2023-02-10 14:38:54 -0300

    ProductCard horizontal responsive

--- frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -16,7 +16,7 @@ function ProductCrudCard( {product} : Props ) {
                 <img src={product.imgUrl} alt="Imagem do produto" />
             </div>
 
-            <div>
+            <div className='product-crud-card-description'>
 
               <div className='product-crud-card-bottom-container'>
                   <h6>{product.name}</h6>
@@ -32,7 +32,7 @@ function ProductCrudCard( {product} : Props ) {
             </div>
 
             <div className='product-crud-card-buttons-container'>
-                <button className='btn btn-outline-danger product-crud-card-button' style={{marginRight:"10px"}}>
+                <button className='btn btn-outline-danger product-crud-card-button product-crud-card-button-first'>
                   EXCLUIR
                 </button>
                 <button className='btn btn-outline-secondary product-crud-card-button'>
